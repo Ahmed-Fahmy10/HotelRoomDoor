b8fbd4c493bbe41150c07a89c1808b233c860863
Ahmed Fahmy <31390580+Ahmed-Fahmy10@users.noreply.github.com>
2020-06-06 23:14:50 +0200

    Add files via upload

---
+++ Objects/config.d
@@ -0,0 +1,4 @@
+.\objects\config.o: config.c
+.\objects\config.o: config.h
+.\objects\config.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\config.o: tm4c123gh6pm.h
---
+++ Objects/main.d
@@ -0,0 +1,7 @@
+.\objects\main.o: main.c
+.\objects\main.o: main.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: std_types.h
+.\objects\main.o: tm4c123gh6pm.h
+.\objects\main.o: uart.h
+.\objects\main.o: config.h
